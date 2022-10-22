66d7599d8738860474b8eba0ae0a9091e9514c86
athensclub <43963760+athensclub@users.noreply.github.com>
2022-10-22 16:39:35 +0700

    Remove random text theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,5 @@ void main() => runApp(MaterialApp(
             backgroundColor: const Color.fromARGB(255, 255, 130, 220),
             foregroundColor: Colors.black),
       ),
-
-
-      // Define the default `TextTheme`. Use this to specify the default
-      // text styling for headlines, titles, bodies of text, and more.
-      textTheme: const TextTheme(
-        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-      ),
     ),
-    home: Create()));
+    home: const Create()));
