a9da75193ea6f8025c03623eae5774561b3a3204
Zenith <sqoulo11@gmail.com>
2022-10-22 14:45:40 +0700

    change main + add asset

---
+++ lib/page/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(5.0, 26.0, 12.0, 8.0),
+        child: Row(
+          children: [
+            Icon()
+          ],
+        ),
+      ),
+    );
+  }
+}
