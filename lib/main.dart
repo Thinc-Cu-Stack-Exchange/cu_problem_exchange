11ae6336e9742879f3223e26c8ec573b04449b02
athensclub <43963760+athensclub@users.noreply.github.com>
2022-10-22 16:24:57 +0700

    Add theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:cu_problem_exchange/page/create.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:cu_problem_exchange/page/home.dart';
 
 void main() => runApp(MaterialApp(
-  home : Home()
-));
+    theme: ThemeData(
+      // Define the default brightness and colors.
+      primaryColor: const Color.fromARGB(255, 255, 130, 220),
+      
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: TextButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 255, 130, 220),
+            foregroundColor: Colors.black),
+      ),
 
+
+      // Define the default `TextTheme`. Use this to specify the default
+      // text styling for headlines, titles, bodies of text, and more.
+      textTheme: const TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      ),
+    ),
+    home: Create()));
