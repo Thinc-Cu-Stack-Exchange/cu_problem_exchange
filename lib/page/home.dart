7e62794d0423a9f96a67f0893e00006aace8bacb
athensclub <43963760+athensclub@users.noreply.github.com>
2022-10-22 21:49:42 +0700

    Linting + Switch to GetX state manager

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:cu_problem_exchange/widget/tabbar.dart';
+import 'package:get/get.dart';
 
-class Home extends StatelessWidget {
+class Home extends GetView<HomeController> {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomTopBar(tag: 'Home',),
-      drawer: Drawer(
+      drawer: const Drawer(
 
       ),
       bottomNavigationBar: CustomBotBar(),
 
-      backgroundColor: Color(0xffE7E7E7),
+      backgroundColor: const Color(0xffE7E7E7),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child : Icon(
+        onPressed: (){},
+        backgroundColor: const Color(0xffe897af),
+        child : const Icon(
           Icons.add,
         ),
-        onPressed: (){},
-        backgroundColor: Color(0xffe897af),
       )
     );
+  }
+}
+
+class HomeController extends GetxController{
+
+}
+
+class HomeBindings implements Bindings{
+  @override
+  void dependencies() {
+    Get.lazyPut(() => HomeController());
   }
 }
