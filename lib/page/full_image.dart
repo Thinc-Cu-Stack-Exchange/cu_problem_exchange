2246b6576dd2550ae8806dcd5255cb46e28e1b10
athensclub <43963760+athensclub@users.noreply.github.com>
2022-10-24 22:07:46 +0700

    Add close button in full image page

--- lib/page/full_image.dart
+++ lib/page/full_image.dart
@@ -1,3 +1,4 @@
+import 'package:cu_problem_exchange/route_names.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:photo_view/photo_view_gallery.dart';
@@ -8,25 +9,26 @@ class FullImages extends GetView<FullImagesController> {
 
   @override
   Widget build(BuildContext context) {
-    
-    controller.currentIndex.value = Get.arguments.initialIndex+1;
+    controller.currentIndex.value = Get.arguments.initialIndex + 1;
     return Scaffold(
       body: Container(
         constraints: BoxConstraints.expand(
           height: context.height,
         ),
         child: Stack(
-          alignment: Alignment.bottomRight,
           children: <Widget>[
             PhotoViewGallery.builder(
-              pageController: PageController(initialPage: Get.arguments.initialIndex),
+              pageController:
+                  PageController(initialPage: Get.arguments.initialIndex),
               scrollPhysics: const BouncingScrollPhysics(),
               builder: (context, index) => PhotoViewGalleryPageOptions(
                   imageProvider: Get.arguments.imageList[index]),
               itemCount: Get.arguments.imageList.length,
               onPageChanged: controller.onPageChanged,
             ),
-            Obx(() => Container(
+            Obx(() => Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
                   padding: const EdgeInsets.all(20.0),
                   child: Text(
                     "${controller.currentIndex}/${Get.arguments.imageList.length}",
@@ -34,7 +36,18 @@ class FullImages extends GetView<FullImagesController> {
                       color: Colors.white,
                     ),
                   ),
-                ))
+                ))),
+            Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                padding: const EdgeInsets.only(top: 45, left: 7),
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ),
+                onPressed: controller.onClosePressed,
+              ),
+            )
           ],
         ),
       ),
@@ -54,5 +67,9 @@ class FullImagesController extends GetxController {
 
   void onPageChanged(int index) {
     currentIndex.value = index + 1;
+  }
+
+  void onClosePressed() {
+    Get.back();
   }
 }
