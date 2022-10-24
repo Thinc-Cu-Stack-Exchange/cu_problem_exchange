e0519312c7de7e45c776902c63ee5c5365374673
Zenith <sqoulo11@gmail.com>
2022-10-24 21:55:49 +0700

    edit align comment

--- lib/page/main_post.dart
+++ lib/page/main_post.dart
@@ -196,7 +196,7 @@ class CreateComment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
